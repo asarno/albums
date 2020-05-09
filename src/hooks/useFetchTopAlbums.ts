9d2b4a47d53dd8314078a3128cc26406a05197ec
asarno <asarno17@gmail.com>
2020-05-09 10:10:54 -0700

    refactor fetchTopAlbums hook to return just necessary info UI

--- src/hooks/useFetchTopAlbums.ts
+++ src/hooks/useFetchTopAlbums.ts
@@ -2,16 +2,38 @@ import { useState, useEffect } from 'react';
 import ALBUMS_DATA from '../resources/data.json';
 
 const simulateAjaxRequest = () => new Promise((resolve, reject) => {
+  const data = ALBUMS_DATA.feed.entry.map((album: any, index: number) => {
+    const name: string = album['im:name'].label;
+    const artist: string = album['im:artist'].label;
+    const price: string = album['im:price'].label;
+    const rights: string = album.rights.label;
+    const url: string = album.link.attributes.href;
+    const image: string = album['im:image'][2].label;
+    const id: string = album.id.attributes["im:id"];
+
+    // create a front end friendly item that just returns what i need :)
+    return {
+      id,
+      name,
+      artist,
+      price,
+      rights,
+      url,
+      image,
+      index,
+    }
+  });
+
   window.setTimeout(
-    () => resolve(ALBUMS_DATA),
-    2000
+    () => resolve(data),
+    500
   );
 });
 
 export function useFetchTopAlbums() {
   const [response, setResponse] = useState(null);
   const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const doFetch = async () => {
